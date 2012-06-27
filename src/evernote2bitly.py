9c361f2c83fc8a403c26b65027749f2190183f1c
Hyeonje Alex Jun <me@noraesae.net>
2012-06-27 15:57:28 +0900

    First working version.
    Committed by noraesae.

--- src/evernote2bitly.py
+++ src/evernote2bitly.py
@@ -1,8 +1,28 @@
 from bs4 import BeautifulSoup as bs
+from datetime import datetime as dt
+import requests
 import sys
+import os
+import time
 
 class Link:
     title = ''
+    url = ''
+    created = ''
+
+def evtime2epoch(evdatetime):
+    evdate = evdatetime.split('T')[0][:8]
+    evtime = evdatetime.split('T')[1][:6]
+
+    year = int(evdate[0:4])
+    month = int(evdate[4:6])
+    day = int(evdate[6:8])
+
+    hour = int(evtime[0:2])
+    minute = int(evtime[2:4])
+    second = int(evtime[4:6])
+
+    return int(time.mktime(dt(year, month, day, hour, minute, second).timetuple()))
 
 # standby
 evernotefile = sys.argv[1]
@@ -21,8 +41,22 @@ for note in soup.find_all('note'):
 # sort by created datetime
 links = sorted(links, key=lambda link: link.created)
 
+
+# get oauth token
+bitly_id = raw_input('bitly account id > ')
+bitly_pw = raw_input('bitly password > ')
+access_token = requests.post('https://api-ssl.bitly.com/oauth/access_token', auth=(bitly_id, bitly_pw)).text
+print access_token
+
+# save url
 for link in links:
+    print '----'
     print link.title
     print link.url
+    print '----'
 
-print len(links)
+    params = {'access_token':access_token, 'longUrl':link.url, 'title':link.title, 'user_ts':str(evtime2epoch(link.created))}
+    r = requests.get('https://api-ssl.bitly.com/v3/user/link_save', params=params)
+
+# print result
+print 'Total '+str(len(links))+' link(s) saved.'
